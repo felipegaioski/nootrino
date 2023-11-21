5b670422d2661fb014cac8232c5be2021868141f
felipegaioski <felipegaioski@gmail.com>
2023-11-21 14:15:29 -0300

    finalizado ok

--- firebase-config.js
+++ firebase-config.js
@@ -19,14 +19,13 @@ const firebaseConfig = {
   // appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   // measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBaT-mtiRqn2QmUqHz9Z-O1MC8joOIV0_g",
+  authDomain: "nootrino-13e7c.firebaseapp.com",
+  projectId: "nootrino-13e7c",
+  storageBucket: "nootrino-13e7c.appspot.com",
+  messagingSenderId: "200932951269",
+  appId: "1:200932951269:web:a28795976bd63ccea1c00c",
+  measurementId: "G-QJGY8E108K"
 };
 
 // Initialize Firebase
