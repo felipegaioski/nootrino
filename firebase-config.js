d5a72ed56fa4d7fd59bf02f4e6357c9d5cef8b74
felipegaioski <felipegaioski@gmail.com>
2023-11-21 13:57:15 -0300

    teste firebase 3

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@
 // nootrino
 import { initializeApp } from "firebase/app";
 //import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from '@firebase/firestore';
+import { getFirestore, initializeFirestore } from '@firebase/firestore';
 //import { getAuth } from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -30,7 +30,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, {
+export const db = initializeFirestore(app, {
   experimentalForceLongPolling: true,
 });
 //export const auth = getAuth(app);
