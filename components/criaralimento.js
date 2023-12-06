c306a4d6a1de2f8ee0d444c0fac79e9f581f2607
felipegaioski <felipegaioski@gmail.com>
2023-12-06 01:09:23 -0300

    criar alimentos select

--- components/criaralimento.js
+++ components/criaralimento.js
@@ -2,13 +2,42 @@ import React, { useState } from 'react';
 import { db } from '../firebase-config';
 import { collection, addDoc, getDocs } from 'firebase/firestore';
 import 'firebase/firestore';
+import Select from 'react-select'
 
-const CriarAlimento = ({ onUpdate }) => {
+const CriarAlimento = () => {
     const [newNome, setNewNome] = useState("");
     const [newQuantidade, setNewQuantidade] = useState(0);
     const [newPorcao, setNewPorcao] = useState(0);
     const [newUnidade, setNewUnidade] = useState("");
     const [newCalorias, setNewCalorias] = useState(0);
+    const [selectedUnidade, setSelectedUnidade] = useState(null);
+    const options = [
+
+        { value: 'opcao1', label: 'Barra(s)' },
+        { value: 'opcao2', label: 'Colher(es) de Chá' },
+        { value: 'opcao3', label: 'Colher(es) Média(s)' },
+        { value: 'opcao4', label: 'Colher(es) de Sopa' },
+        { value: 'opcao5', label: 'Colher(es) de Servir' },
+        { value: 'opcao6', label: 'Copo(s) Grande(s)' },
+        { value: 'opcao7', label: 'Copo(s) Pequeno(s)' },
+        { value: 'opcao8', label: 'Fatia(s) Pequena(s)' },
+        { value: 'opcao9', label: 'Fatia(s) Fina(s)' },
+        { value: 'opcao10', label: 'Fatia(s) Média(s)' },
+        { value: 'opcao11', label: 'Fatia(s) Grande(s)' },
+        { value: 'opcao12', label: 'Folha(s)' },
+        { value: 'opcao13', label: 'Gomo(s)' },
+        { value: 'opcao14', label: 'Lata(s)' },
+        { value: 'opcao15', label: 'Pacote(s)' },
+        { value: 'opcao16', label: 'Ramo(s)' },
+        { value: 'opcao17', label: 'Unidade(s) Pequena(s)' },
+        { value: 'opcao18', label: 'Unidade(s) Média(s)' },
+        { value: 'opcao19', label: 'Unidade(s) Grande(s)' }
+
+    ];
+
+    const handleSelectedOption = (selectedOption) => {
+        setSelectedUnidade(selectedOption);
+    };
 
     const foodColletctionRef = collection(db, "alimentos")
 
@@ -18,7 +47,6 @@ const CriarAlimento = ({ onUpdate }) => {
             newNome.trim() === "" ||
             newQuantidade <= 0 ||
             newPorcao <= 0 ||
-            newUnidade.trim() === "" ||
             newCalorias <= 0
         ) {
             alert("Por favor, preencha todos os atributos do alimento");
@@ -32,7 +60,13 @@ const CriarAlimento = ({ onUpdate }) => {
         if (hasDuplicate) {
             alert(`Já existe um alimento com o nome ${newNome}!`);
         } else {
-            await addDoc(foodColletctionRef, { nome: newNome, quantidade: Number(newQuantidade), porcao: Number(newPorcao), unidade: newUnidade, calorias: newCalorias });
+            await addDoc(foodColletctionRef, {
+                nome: newNome,
+                quantidade: Number(newQuantidade),
+                porcao: Number(newPorcao),
+                unidade: selectedUnidade.label,
+                calorias: newCalorias
+            });
             onUpdate();
         };
     }
@@ -49,7 +83,11 @@ const CriarAlimento = ({ onUpdate }) => {
             <input type="number" placeholder="Medida caseira (número)" onChange={(event) => { setNewPorcao(event.target.value) }} />
             <br></br>
             <label>Unidade</label>
-            <input placeholder="Unidade (ex: fatias, colheres)" onChange={(event) => { setNewUnidade(event.target.value) }} />
+            <Select placeholder="Selecione a unidade"
+                value={selectedUnidade}
+                onChange={handleSelectedOption}
+                options={options}
+            />
             <br></br>
             <label>Quantidade de calorias</label>
             <input type="number" placeholder="Calorias" onChange={(event) => { setNewCalorias(event.target.value) }} />
--- components/criaralimentohome.js
+++ components/criaralimentohome.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { db } from '../firebase-config';
 import { collection, addDoc, getDocs } from 'firebase/firestore';
 import 'firebase/firestore';
+import Select from 'react-select'
 
 const CriarAlimento = () => {
     const [newNome, setNewNome] = useState("");
@@ -9,7 +10,34 @@ const CriarAlimento = () => {
     const [newPorcao, setNewPorcao] = useState(0);
     const [newUnidade, setNewUnidade] = useState("");
     const [newCalorias, setNewCalorias] = useState(0);
+    const [selectedUnidade, setSelectedUnidade] = useState(null);
+    const options = [
 
+        { value: 'opcao1', label: 'Barra(s)' },
+        { value: 'opcao2', label: 'Colher(es) de Chá' },
+        { value: 'opcao3', label: 'Colher(es) Média(s)' },
+        { value: 'opcao4', label: 'Colher(es) de Sopa' },
+        { value: 'opcao5', label: 'Colher(es) de Servir' },
+        { value: 'opcao6', label: 'Copo(s) Grande(s)' },
+        { value: 'opcao7', label: 'Copo(s) Pequeno(s)' },
+        { value: 'opcao8', label: 'Fatia(s) Pequena(s)' },
+        { value: 'opcao9', label: 'Fatia(s) Fina(s)' },
+        { value: 'opcao10', label: 'Fatia(s) Média(s)' },
+        { value: 'opcao11', label: 'Fatia(s) Grande(s)' },
+        { value: 'opcao12', label: 'Folha(s)' },
+        { value: 'opcao13', label: 'Gomo(s)' },
+        { value: 'opcao14', label: 'Lata(s)' },
+        { value: 'opcao15', label: 'Pacote(s)' },
+        { value: 'opcao16', label: 'Ramo(s)' },
+        { value: 'opcao17', label: 'Unidade(s) Pequena(s)' },
+        { value: 'opcao18', label: 'Unidade(s) Média(s)' },
+        { value: 'opcao19', label: 'Unidade(s) Grande(s)' }
+
+    ];
+
+    const handleSelectedOption = (selectedOption) => {
+        setSelectedUnidade(selectedOption);
+    };
     const foodColletctionRef = collection(db, "alimentos")
 
     const resetForm = () => {
@@ -26,7 +54,6 @@ const CriarAlimento = () => {
             newNome.trim() === "" ||
             newQuantidade <= 0 ||
             newPorcao <= 0 ||
-            newUnidade.trim() === "" ||
             newCalorias <= 0
         ) {
             alert("Por favor, preencha com atributos válidos");
@@ -40,7 +67,13 @@ const CriarAlimento = () => {
         if (hasDuplicate) {
             alert(`Já existe um alimento com o nome ${newNome}!`);
         } else {
-            await addDoc(foodColletctionRef, { nome: newNome, quantidade: Number(newQuantidade), porcao: Number(newPorcao), unidade: newUnidade, calorias: newCalorias });
+            await addDoc(foodColletctionRef, {
+                nome: newNome,
+                quantidade: Number(newQuantidade),
+                porcao: Number(newPorcao),
+                unidade: selectedUnidade.label,
+                calorias: newCalorias
+            });
         };
 
         resetForm();
@@ -49,6 +82,7 @@ const CriarAlimento = () => {
         document.getElementById("input-porcao").value = "";
         document.getElementById("input-unidade").value = "";
         document.getElementById("input-calorias").value = "";
+
     }
 
     return (
@@ -63,7 +97,11 @@ const CriarAlimento = () => {
             <input id="input-porcao" type="number" placeholder="Medida caseira (número)" onChange={(event) => { setNewPorcao(event.target.value) }} />
             <br></br>
             <label>Unidade</label>
-            <input id="input-unidade" placeholder="Unidade (ex: fatias, colheres)" onChange={(event) => { setNewUnidade(event.target.value) }} />
+            <Select placeholder="Selecione a unidade"
+                value={selectedUnidade}
+                onChange={handleSelectedOption}
+                options={options}
+            />
             <br></br>
             <label>Quantidade de calorias</label>
             <input id="input-calorias" type="number" placeholder="Calorias" onChange={(event) => { setNewCalorias(event.target.value) }} />
